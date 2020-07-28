449fefc77b7f5e602fb0eec2a0b30c50e47e89e3
kito-kid <68926691+kito-kid@users.noreply.github.com>
2020-07-28 23:52:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 
-        //<![CDATA[
+       //<![CDATA[
             function nocontext(e) {var clickedTag = (e==null) ? event.srcElement.tagName : e.target.tagName; if (clickedTag == "IMG") {alert(alertMsg);return false;}} var alertMsg = "الصور عليها حقوق لذلك ممنوع النسخ";document.oncontextmenu = nocontext; 
+
+            var name = localStorage.getItem("name")
