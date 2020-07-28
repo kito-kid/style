3d9702d47e55938b32bce1c481ee508a47e939e8
kito-kid <68926691+kito-kid@users.noreply.github.com>
2020-07-28 23:41:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+
+        //<![CDATA[
+            function nocontext(e) {var clickedTag = (e==null) ? event.srcElement.tagName : e.target.tagName; if (clickedTag == "IMG") {alert(alertMsg);return false;}} var alertMsg = "الصور عليها حقوق لذلك ممنوع النسخ";document.oncontextmenu = nocontext; 
